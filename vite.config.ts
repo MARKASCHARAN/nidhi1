fd38119abf17ea4bf30a4eba8b8fef220c5dc861
MARKASCHARAN <markascharan@gmail.com>
2025-01-03 16:50:13 +0530

    add

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nidhi1/', // Replace 'nidhi' with your repository name
+  base: '/', // Use '/' for custom domain deployment
 });
